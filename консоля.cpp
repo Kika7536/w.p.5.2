e9b6e4c41fa9ca0f4bff641b3ec0585e7a484f33
Kika7536 <104264433+Kika7536@users.noreply.github.com>
2024-09-19 23:22:47 +0300

    Добавьте файлы проекта.

---
+++ консоля.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int number, digit = 0;
+	
+
+	cout << "Задание 2. Слишком большие числа." << endl << "--------------------------------" << endl << endl;
+
+
+	
+
+	
+
+		cout << "Введите число: ";
+		cin >> number;
+		
+
+
+		while (true) {
+
+			if (number / 10 > 0 || number / 10 < 0) {
+				digit++;
+				number /= 10;
+			}
+			else {
+				digit++;
+				break;
+			}
+		}
+
+		cout << "Цифр в числе: " << digit << endl;
+		
+		
+		
+
+	
+
+}
